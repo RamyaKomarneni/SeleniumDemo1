de01619454ce1de3698be85c3beafc715b5276cb
RamyaKomarneni <165860644+RamyaKomarneni@users.noreply.github.com>
2024-04-02 21:09:35 -0500

    Update TC_01.java
    Updated test case 1

--- Demo1/src/com/selenium/demo/TC_01.java
+++ Demo1/src/com/selenium/demo/TC_01.java
@@ -4,7 +4,7 @@ public class TC_01 {
 
 	public static void main(String[] args) {
 		System.out.println("Test case 1");
-		System.out.println(" New Test cas");e
+		System.out.println(" New Test cas");
 	}
 
 }
